0e676aa62dfe04a8e6e9a15801d8d1a2f8ff4dff
ha-ayoub <hafdi.ayoub@gmail.com>
2025-10-19 17:17:53 +0200

    Add animation score

--- src/components/ScoredCard.jsx
+++ src/components/ScoredCard.jsx
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react"
+
 export default function ScoredCard({ label, score, type }) {
+
+    const [isAnimateScore, setIsAnimateScore] = useState(false);
+
+    useEffect(() => {
+        console.log(score);
+        if(score !== 0){
+            console.log('enter');
+            setIsAnimateScore(true);
+            const timeout = setTimeout(()=> setIsAnimateScore(false), 1000);
+            return ()=> clearTimeout(timeout);
+        }
+    }, [score]);
+
     return (
         <div className={`score-card ${type}-score`}>
             <div className="score-label"> {label} </div>
-            <div className="score-value"> {score} </div>
+            <div className={`score-value ${isAnimateScore ? "animate" : "" }`}> {score} </div>
         </div>
     )
 }
